ff346d723a5ba0d0d47ffd3eb8944dc0ede49ec9
khizar bajwa <kbajwa@ssilhr.com.pk>
2021-06-30 04:27:33 +0500

    update

--- src/app/landingpages/services/services.component.ts
+++ src/app/landingpages/services/services.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {treatments} from "../services/servicesdata.json";
+import { FormControl } from '@angular/forms';
+
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-services',
@@ -7,8 +12,15 @@ import {treatments} from "../services/servicesdata.json";
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent implements OnInit {
-  public data = treatments;
-  constructor() { }
+  public data: any;
+
+  public configUrl = 'https://www.dmb.somee.com/api/dmb/';
+  constructor(private http: HttpClient) {
+    this.http.get(this.configUrl+'services').subscribe((data)=>{
+      debugger;
+      this.data = data['data'];
+ });
+   }
 
   ngOnInit(): void {
 
